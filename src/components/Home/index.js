9e44c0d20cf4b2d00c7d03b5db5e69f073cd4c1c
Priyanshu <myselfpriyanshu1@gmail.com>
2024-01-09 17:23:00 +0530

    Priyanshu logo set

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,9 +2,7 @@ import { Link } from 'react-router-dom'
 import LogoTitle from '../../assests/images/logo-s.png'
 import './index.scss';
 const Home = () => {
-   /* const [letterClass,setLetterClass]= useState('text-animate')
-    const nameArray =['P','R','I','Y','A','N','H','U']
-    const jobArray =['w','e','b',' ','d','e','v','e','l','o','p','e','r','.'] */
+   
     return(
         <div className="container home-page">
             <div className="text-zone" >
