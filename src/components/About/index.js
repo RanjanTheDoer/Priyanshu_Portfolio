aa41d779c0a3e614b3f4cf5bbe2441b89324b618
Priyanshu <myselfpriyanshu1@gmail.com>
2024-01-12 11:32:06 +0530

    page loader animation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useState,useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 const About = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -33,6 +34,7 @@ const About = () => {
 
 
     return (
+        <>
         <div className="container about-page">
             <div className="text-zone">
             <h1>
@@ -91,6 +93,8 @@ const About = () => {
             </div>
 
         </div>
+        <Loader type="pacman"/>
+        </>
     )
 }
 
